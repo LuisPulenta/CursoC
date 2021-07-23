0749162b4da58150183f6917215e8deae1c2f94e
luisalbertonu <luisalbertonu@gmail.com>
2021-07-23 18:55:46 -0300

    Fin Cap 1

---
+++ CursoC/01-HolaMundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01_HolaMundo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hola Mundo!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CursoC/02-Comentarios/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01_HolaMundo
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Este es un comentario simple
+            //La línea escribe 7
+            Console.WriteLine(5+2);
+
+            /* 
+            Esto es un
+            comentario complejo
+            que puede utilizar varias líneas 
+            */
+            Console.WriteLine("Luis");
+            Console.ReadLine();
+
+            //Con Control KC se comentan varias líneas
+            //Con Control KU se descomentan varias líneas
+        }
+    }
+}
